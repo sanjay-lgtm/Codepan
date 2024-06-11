5119456e5491688e36fb01b436da8a4a11d5e56a
sanjay <113435192+sanjay-lgtm@users.noreply.github.com>
2024-06-12 00:14:01 +0530

    Login page

---
+++ src/component/UserAuthInput.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { motion } from 'framer-motion';
+const UserAuthInput = ({ label, placeHolder, isPass, setStateFunction, Icon ,setGetEmailValidationState}) => {
+    const [value, setValue] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
+    const [isEmailValid, setIsEmailValid] = useState(false)
+    const handleInputChange = (e) => {
+        const newValue = e.target.value;
+        setValue(newValue);
+        setStateFunction(newValue);
+
+        if (!placeHolder === "Email") {
+            const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+            const status = emailRegex.test(value)
+            setIsEmailValid(status);
+            setGetEmailValidationState(status)
+        }
+    };
+
+    const toggleShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
+
+    return (
+        <div className='flex flex-col items-start justify-start gap-1'>
+            <label className='text-sm text-gray-300'>{ label }</label>
+            <div className={ `flex items-center justify-center gap-3 w-full md:w-96 rounded-md px-4 py-1 bg-gray-200 ${!isEmailValid && placeHolder === "Email" &&
+                value.length > 0 &&
+                "border-2 border-red-500"
+                }` }>
+                <Icon className='text-text555 text-2xl' />
+                <input
+                    type={ isPass && !showPassword ? "password" : "text" }
+                    placeholder={ placeHolder }
+                    className='flex-1 w-full h-full outline-none border-none bg-transparent text-text555 text-lg'
+                    value={ value }
+                    onChange={ handleInputChange }
+                />
+                { isPass && (
+                    <motion.div whileTap={ { scale: 0.9 } } className='cursor-pointer' onClick={ toggleShowPassword }>
+                        { showPassword ? (
+
+                            <FaEyeSlash className='text-text555 text-2xl cursor-pointer' />
+                        ) : (
+
+                            <FaEye className='text-text555 text-2xl cursor-pointer' />
+                        ) }
+                    </motion.div>
+                ) }
+            </div>
+        </div>
+    );
+};
+
+export default UserAuthInput;
--- src/container/SignUp.jsx
+++ src/container/SignUp.jsx
@@ -1,11 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Logo from '../assets/Logo.webp'; // Ensure correct import path and file extension
+import { UserAuthInput } from '../component';
+import { FaEnvelope } from 'react-icons/fa';
+import { MdPassword } from 'react-icons/md';
+import { motion } from 'framer-motion';
+import { FcGoogle } from "react-icons/fc";
 
 const SignUp = () => {
-  return (
-    <div className='text-primaryText'>
-      SignUp
-    </div>
-  )
-}
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [getEmailValidationState, setGetEmailValidationState] = useState(false)
 
-export default SignUp
+    const [isLogin, setIsLogin] = useState(false)
+    return (
+        <div className='w-full py-6'>
+            <img src={ Logo } alt='Logo' className='object-contain w-32 h-auto opacity-90' />
+
+            <div className='w-full flex flex-col items-center justify-center py-8'><p className='py-12 text-2xl text-primaryText'>Join With Us! 😊</p>
+                <div className='px-8 w-full md:w-auto py-4 rounded-xl bg-secondary shadow-md flex flex-col items-center justify-center gap-8'>
+
+                    {/* email */ }
+                    <UserAuthInput label="Email" placeHolder="Email" isPass={ false } key="Email" setStateFunction={ setEmail } Icon={ FaEnvelope } setGetEmailValidationState={ setGetEmailValidationState } />
+
+                    {/* password */ }
+                    <UserAuthInput label="Password" placeHolder="********" isPass={ true } key="Password" setStateFunction={ setPassword } Icon={ MdPassword } />
+
+                    {/* alert section  */ }
+
+                    {/* login button  */ }
+                    { !isLogin ? (<motion.div
+                        whileTap={ { scale: 0.9 } }
+                        className='flex items-center justify-center w-full py-3 rounded-md bg-emerald-500    text-white  cursor-pointer hover:bg-emerald-700'>
+                        <p className='text-xl text-white'>SignUp</p>
+                    </motion.div>) : (
+                        <motion.div
+                            whileTap={ { scale: 0.9 } }
+                            className='flex items-center justify-center w-full py-3 rounded-md bg-emerald-500    text-white  cursor-pointer hover:bg-emerald-700'>
+                            <p className='text-xl text-white'>Login</p>
+                        </motion.div>
+                    ) }
+                    {/* text  */ }
+                   {!isLogin ?(
+                     <p className='text-sm text-primaryText flex items-center justify-center gap-1'>Already have an account ! <span onClick={() => setIsLogin(!isLogin)} className='text-emerald-500 gap-8'>LogIn here</span></p>
+                   ):(
+                    <p className='text-sm text-primaryText flex items-center justify-center gap-1'>Does't have an account ! <span  onClick={() => setIsLogin(!isLogin)}  className='text-emerald-500 gap-8'>Create here</span></p>
+                   )}
+                    {/* or  */ }
+                   <div className='flex items-center justify-center gap-12'>
+                   <div className='h-[1px] bg-primaryText rounded-md w-24'></div>
+                   <p className='text-sm text-primaryText'>OR</p>
+                   <div className='h-[1px] bg-primaryText rounded-md w-24'></div>
+                   </div>
+                    {/* google signin  */ }
+                   <motion.div whileTap={{scale:0.9}}>
+                   <FcGoogle />
+
+                   </motion.div>
+
+                    {/* or  */ }
+                    <div className='flex items-center justify-center gap-12'>
+                   <div className='h-[1px] bg-primaryText rounded-md w-24'></div>
+                   <p className='text-sm text-primaryText'>OR</p>
+                   <div className='h-[1px] bg-primaryText rounded-md w-24'></div>
+                   </div>
+
+                    {/* github */ }
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default SignUp;
